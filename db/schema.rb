b3ae888a40b0e8b57548280c377e00b6ac5b5f8e
tuckyeah <h.rosebrock@gmail.com>
2016-09-29 16:17:08 -0400

    Add logics model with empty placeholder methods

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929160441) do
+ActiveRecord::Schema.define(version: 20160929192926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,11 @@ ActiveRecord::Schema.define(version: 20160929160441) do
   end
 
   add_index "games", ["user_id"], name: "index_games_on_user_id", using: :btree
+
+  create_table "logics", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
