cb8b5aa77c23897aed08cc230506209c4011db80
tuckyeah <h.rosebrock@gmail.com>
2016-09-29 09:27:30 -0400

    Refactor seed data code and add CURL scripts to GET /categories
    Add more categories/data to small_jeopardy.csv, and refactor
    seed data code to streamline database population and ignore
    any clues with media/links. Add CURL scripts for GET index and show
    actions to Categories in scripts folder also.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,37 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'csv'
 
-CSV.foreach('lib/seeds/small_jeopardy.csv',
-            headers: true,
-            header_converters: -> (h) { h.lstrip.downcase.to_sym }) do |row|
-              c = row.to_h
-              c[:value].rstrip!
-              if Category.all.where(name: c[:category]).length == 1 # there's gotta be a better way to do this
-                category = Category.all.where(name: c[:category])
-              else
-                category = Category.create([{ name: c[:category] }])
+# REFACTOR THIS FURTHER we can make this better.
+# returns an array with each row of the file converted to a hash
+def convert_to_hash
+  arr = []
+  CSV.foreach('lib/seeds/small_jeopardy.csv',
+              headers: true,
+              header_converters: -> (h) { h.lstrip.downcase.to_sym }) do |row|
+                arr << row.to_h
               end
-              Clue.create([{ question: c[:question], answer: c[:answer],
-                             value: c[:value], category_id: category.first.id }])
-            end
+  arr
+end
+
+def create_categories(data)
+  data.each do |hsh|
+    categories = Category.all
+    unless categories.where(name: hsh[:category]).length == 1
+      Category.create([{ name: hsh[:category] }])
+    end
+  end
+end
+
+def populate_categories(data)
+  data.each do |hsh|
+    category_id = Category.where(name: hsh[:category]).ids[0]
+    unless hsh[:question].include? '<a href=' or hsh[:answer].include? '<a href='
+      Clue.create([{ question: hsh[:question], answer: hsh[:answer],
+                     value: hsh[:value].rstrip, category_id: category_id }])
+    end
+  end
+end
+
+data = convert_to_hash
+create_categories(data)
+populate_categories(data)
---
+++ scripts/categories.sh
@@ -0,0 +1,10 @@
+# GET index
+curl --include --request GET 'http://localhost:3000/categories'
+
+# GET show
+curl --include --request GET "http://localhost:3000/categories/3"
+# POST
+
+# PATCH
+
+# DELETE
