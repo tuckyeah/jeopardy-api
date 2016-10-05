ec2d68785580ec0b0863e5990219d99eb68bc9ba
tuckyeah <h.rosebrock@gmail.com>
2016-10-04 21:48:31 -0400

    change client origin

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module RailsApiJeopardy
     # Cross-Origin Resource Sharing
     config.middleware.use Rack::Cors do
       allow do
-        origins ENV['CLIENT_ORIGIN'] || 'http://localhost:8080'
+        origins ENV['CLIENT_ORIGIN'] || 'https://tuckyeah.github.io/jeopardy-client/'
         resource '*',
                  headers: :any,
                  methods: [:options, :get,
