bea85d0df5165b8a0c047824734e7be17c4ea309
tuckyeah <h.rosebrock@gmail.com>
2016-11-16 17:02:22 -0500

    Refactor seed data to pick from master CSV file
    Seed data now picks 50 categories at random from the CSV file
    each time it is seeded.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ require 'csv'
 # returns an array with each row of the file converted to a hash
 def convert_to_hash
   arr = []
-  CSV.foreach('lib/seeds/small_jeopardy.csv',
+  CSV.foreach('lib/seeds/JEOPARDY_CSV.csv',
               headers: true,
               header_converters: -> (h) { h.lstrip.downcase.to_sym }) do |row|
                 arr << row.to_h
@@ -41,80 +41,25 @@ value_hash.reject! { |k, _v| value_hash[k].length < 5 }
 res = category_hash.merge(value_hash)
 res.reject! { |k, _v| res[k].length < 5 }
 res.reject! { |k, _v| k.is_a? Fixnum }
+res.keys.shuffle!
 
-res.each_key do |category|
+def category_maker(category, clue_vals, val_index, all_vals)
+  return if val_index.nil?
+
+  @category = Category.create([{ name: category }])
+  all_vals[val_index].each do |num|
+    sample = clue_vals[num].sample
+    Clue.create([{ question: sample[:question], answer: sample[:answer],
+                   value: sample[:value], category_id: @category[0][:id] }])
+  end
+end
+
+res.keys.shuffle.each do |category|
   val_index = nil
   clue_vals = res[category].group_by{|clue| clue[:value] } # group that category's clues by their values
   if all_vals.include?(clue_vals.keys) # see if the all values array includes which values those clues are
     val_index = all_vals.find_index(clue_vals.keys)
   end
   category_maker(category, clue_vals, val_index, all_vals)
-  break if CategoryBuilder.all_instances.length == 10
+  break if Category.all.length == 50
 end
-
-class CategoryBuilder
-  attr_accessor :clues, :counter, :name
-  @@counter = []
-  @@names = []
-
-  def initialize(category)
-    @name = category
-    @clues = []
-    @@counter << self
-    @@names << name
-  end
-
-  def add_clue(clue)
-    @clues << clue
-  end
-
-  def self.all_instances
-    @@counter
-  end
-
-  def self.all_names
-    @@names
-  end
-end
-
-category_counter = []
-
-def category_maker(category, clue_vals, val_index, all_vals)
-  return if val_index.nil?
-
-  category = CategoryBuilder.new(category) # replace this with actual category builder and test
-  all_vals[val_index].each do |num|
-    sample = clue_vals[num].sample
-    category.clues << sample # add another test to make sure that the length is at least five or something, or that they're unique
-  end
-
-  puts "Category is: #{category.name}"
-  puts "Clues are: #{category.clues.length}"
-end
-
-
-
-
-# then, as long as val_index is not nil
-# use that category to make a new category
-# and iterate through the corresponding values array, picking one clue from each value
-# and adding it to the category
-
-
-res.each_key do |category|
-  @category = Category.create([{ name: category }])
-  clues = res[category]
-  clue_group = clues.group_by { |clue| clue[:value] }
-  clue_group.each_key do | key |
-    sample = clue_group[key].sample(1).first
-    Clue.create([{ question: sample[:question], answer: sample[:answer],
-                   value: sample[:value], category_id: @category[0][:id] }])
-  end
-end
-
-# categoryBuilder
-# sort of a 'fake' category builder before we build them in rails
-# it will:
-#     try to create make its own category class
-#     make sure that there it has five clues of unique, ascending values
-#     if that works, it will actually make the category and clues
